ae49778681d41a873d915b8e03cbe4fdb8ffb020
Kassudin <kasper.kinnarinen@live.fi>
2025-04-19 22:53:24 +0300

    Väärä import

--- src/tests/generaattori_test.py
+++ src/tests/generaattori_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from generaattori import satunnaiset_pisteet, maaston_vari
+from generaattori import satunnaiset_pisteet, biomin_vari
 
 class TestGeneraattori(unittest.TestCase):
 
@@ -10,36 +10,36 @@ class TestGeneraattori(unittest.TestCase):
 
     def test_syva_vesi(self):
         x = -0.6
-        self.assertEqual(maaston_vari(x), (25, 50, 80)) 
+        self.assertEqual(biomin_vari(x), (25, 50, 80)) 
 
     def test_rantavesi(self):
         x = -0.4
-        self.assertEqual(maaston_vari(x), (80, 120, 150)) 
+        self.assertEqual(biomin_vari(x), (80, 120, 150)) 
     
     def test_hiekkaranta(self):
         x = -0.2
-        self.assertEqual(maaston_vari(x), (210, 200, 160)) 
+        self.assertEqual(biomin_vari(x), (210, 200, 160)) 
     
     def test_laakso(self):
         x = 0.0
-        self.assertEqual(maaston_vari(x), (170, 190, 140))
+        self.assertEqual(biomin_vari(x), (170, 190, 140))
     
     def test_metsa(self):
         x = 0.2
-        self.assertEqual(maaston_vari(x), (140, 170, 120))
+        self.assertEqual(biomin_vari(x), (140, 170, 120))
 
     def test_kukkulat(self):
         x = 0.4
-        self.assertEqual(maaston_vari(x), (120, 100, 80))
+        self.assertEqual(biomin_vari(x), (120, 100, 80))
 
     def test_vuori(self):
         x = 0.6
-        self.assertEqual(maaston_vari(x), (160, 140, 120))
+        self.assertEqual(biomin_vari(x), (160, 140, 120))
     
     def test_lumi(self):
         x = 0.8
-        self.assertEqual(maaston_vari(x), (230, 230, 230))
+        self.assertEqual(biomin_vari(x), (230, 230, 230))
     
-    def test_ulkopuolella(self):
+    def test_raja(self):
         x = 1.0
-        self.assertEqual(maaston_vari(x), (230, 230, 230))
+        self.assertEqual(biomin_vari(x), (230, 230, 230))
