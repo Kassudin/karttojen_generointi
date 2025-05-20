1d03c760aa0caee1146eb530f3cf0899167bdb6f
Kassudin <kasper.kinnarinen@live.fi>
2025-05-21 00:10:33 +0300

    Tyylittelyä

--- src/tests/voronoi_test.py
+++ src/tests/voronoi_test.py
@@ -6,23 +6,23 @@ from voronoi_diagrammi import voronoi, kulma_suhteessa_pisteeseen
 
 class Testvoronoi_diagrammi(unittest.TestCase):
     def test_karki_suhteessa_pisteeseen_nollakulma(self):
-        kulma = kulma_suhteessa_pisteeseen((0,0),(1,0)) #tämä on 0 astetta
+        kulma = kulma_suhteessa_pisteeseen((0,0),(1,0)) # Tämä on 0 astetta.
         self.assertAlmostEqual(kulma, 0)
 
     def test_karki_suhteessa_pisteeseen_pi(self):
-        kulma = kulma_suhteessa_pisteeseen((0,0),(-1,0)) #tämä on pi
+        kulma = kulma_suhteessa_pisteeseen((0,0),(-1,0)) # Tämä on pi.
         self.assertAlmostEqual(kulma, math.pi)
    
-    def test_voronoi_yksi_kolmio(self): #voronoi palauttaa kolmioiden kärjet
+    def test_voronoi_yksi_kolmio(self): # Voronoi palauttaa kolmioiden kärjet.
         kolmio = Kolmio((0,0),(0,2),(2,0))
         tulos = voronoi([kolmio])
-        self.assertEqual(len(tulos), 3) #kolme kärkeä
+        self.assertEqual(len(tulos), 3) # Kolme kärkeä.
   
     def test_yhteinen_karki(self): 
         kolmio = Kolmio((0,0),(0,2),(2,0))
         kolmio2 = Kolmio((2,2),(2,0),(-1,-1)) 
         tulos = voronoi([kolmio, kolmio2])
-        #yhteinen kärki (2,0)
+        # Yhteinen kärki (2,0).
         self.assertEqual(len(tulos), 5)
   
     def test_ei_monikulmioita(self):
@@ -31,6 +31,6 @@ class Testvoronoi_diagrammi(unittest.TestCase):
         tulos = voronoi([kolmio, kolmio2])
         for karki in (kolmio.A, kolmio.B, kolmio.C, kolmio2.A, kolmio2.B, kolmio2.C):
             self.assertIn(karki, tulos)
-            # tarkistetaan, että kärjille ei ole monikulmioita
+            # Tarkistetaan, että kärjille ei ole monikulmioita.
             self.assertEqual(tulos[karki], [])
     
